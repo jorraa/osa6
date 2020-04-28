e078b4368530b9dc7d40e01f5ff855359620a163
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-04-28 17:46:59 +0300

    6.20 tehty

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch()
   
   const addAnecdote = async (event) => {
     event.preventDefault()
@@ -13,8 +12,8 @@ const AnecdoteForm = (props) => {
       votes: 0
     }
     event.target.content.value = ''
-    await dispatch(createAnecdote(newAnecdote))
-    dispatch(setNotification(`${newAnecdote.content} created`, 10))
+    props.createAnecdote(newAnecdote)
+    props.setNotification(`${newAnecdote.content} created`, 10)
   }
 
   return (
@@ -28,4 +27,7 @@ const AnecdoteForm = (props) => {
   )
 }
 
-export default AnecdoteForm
+export default connect(
+  null, 
+  {createAnecdote, setNotification}
+)(AnecdoteForm)
